0c0d2c8851d6b81f0f499f8e1f11b0e1a176163c
Believeharsh <93024harsh@gmail.com>
2023-10-04 13:49:56 +0530

    previous Next button complted

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -13,7 +13,7 @@ export default class NewsItem extends Component {
             <div className="card-body">
               <h5 className="card-title">{title}....</h5>
               <p className="card-text">{descreption}.....</p>
-              <a href={newsurl} target='-blank' className="btn btn-sm btn-primary">Read More</a>
+              <a href={newsurl} target='-blank' className="btn btn-sm btn-info">Read More</a>
             </div>
         </div>
         </div>
