92eba57e99ad0d26909bafcc5d516b27b6945bf0
Believeharsh <93024harsh@gmail.com>
2023-10-04 19:08:31 +0530

    added spinner feature

---
+++ src/Components/Spinner.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Spinner() {
+  return (
+    <div>
+    <div className="spinner-border text-dark text-center" role="status">
+  <span className="sr-only">Loading...</span>
+</div>
+      
+    </div>
+  )
+}
