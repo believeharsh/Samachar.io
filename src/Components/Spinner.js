0ce5f54639bda20707b805aeb0174ff797a1ca97
Believeharsh <93024harsh@gmail.com>
2023-10-05 21:10:39 +0530

    formated spinner.js

--- src/Components/Spinner.js
+++ src/Components/Spinner.js
@@ -3,10 +3,9 @@ import React from 'react'
 export default function Spinner() {
   return (
     <div>
-    <div className="spinner-border text-dark text-center" role="status">
-  <span className="sr-only">Loading...</span>
-</div>
-      
+      <div className="spinner-border text-dark text-center" role="status">
+        <span className="sr-only">Loading...</span>
+      </div>
     </div>
   )
 }
