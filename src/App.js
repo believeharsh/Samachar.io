3fef51c41f37589b6b02b75100bf361e4cdcd486
Believeharsh <93024harsh@gmail.com>
2023-10-07 18:01:21 +0530

    formated with prettier

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import './App.css';
+import "./App.css";
 
-import React, { Component } from 'react'
-import Navbar from './Components/Navbar';
-import News from './Components/News';
+import React, { Component } from "react";
+import Navbar from "./Components/Navbar";
+import News from "./Components/News";
 // import NewsItem from './Components/NewsItem';
-
-
-
 
 export default class App extends Component {
   render() {
     return (
       <div>
-      <Navbar/>
-
-      
-      
-        
-
-        
+        <Navbar />
       </div>
-    )
+    );
   }
 }
-
