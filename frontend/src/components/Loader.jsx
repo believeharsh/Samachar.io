4752c39cebdb9b150e7298c01ed4c6917198fbfc
Believeharsh <bontinue@gmail.com>
2024-08-15 11:19:05 +0530

    renamed the folder

---
+++ frontend/src/components/Loader.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+function Loader() {
+  return (
+    <div className='loader-container w-full absolute flex justify-center top-0 left-0'>
+       
+        <span className="loader"></span>
+    </div>
+  )
+}
+
+export default Loader
